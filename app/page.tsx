982abcfee1e1b7e6703e49522174a31fb52eb85e
happyboy <superhappyboy1995@gmail.com>
2025-12-11 20:56:13 +0800

    update : init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import TradePage from "./pages/trade/page";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen">
+      <TradePage />
+    </div>
+  );
+}
---
+++ app/pages/trade/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function TradePage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white py-12 px-4">
+       sdf 
+    </div>
+  );
+}
